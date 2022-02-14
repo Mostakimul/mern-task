3fb7e1736b0d19adf7731f642fa5eabbfa06863f
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2022-02-13 20:48:35 -0800

    database connection suucessful

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
+// databse connection
+const connectDB = require('./config/db');
+connectDB();
 
+// app initialization
 const app = express();
 
+// routes
 app.use('/api/tasks', require('./routes/taskRoutes'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));

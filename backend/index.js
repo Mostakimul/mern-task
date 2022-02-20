e29daa959fd244dd62506be57c4436eb2c85492c
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2022-02-20 20:02:54 +0100

    setup for deploy

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 const { errorHandler } = require('./middleware/errorMiddleware');
@@ -17,6 +18,19 @@ app.use(express.urlencoded({ extended: false }));
 // routes
 app.use('/api/tasks', require('./routes/taskRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
+
+// serve forntend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'),
+    ),
+  );
+} else {
+  app.get('/', (req, res) => res.send('Task Manager delevelopment!'));
+}
 
 // error handler
 app.use(errorHandler);
